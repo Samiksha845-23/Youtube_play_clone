1dfedf3cbcb365e5fb818faddcc4225b4819ebbd
Samiksha Khandelwal <samikshamau5678@gmail.com>
2023-09-14 18:31:28 +0530

    SideBar in Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar } from './';
 const Feed = () => {
   return (
-    <div>
-      feed
-    </div>
-  )
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #fff",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar></Sidebar>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 @samiksha
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
 export default Feed
